f7700e2825d1177d6f8203820c976be42b2a867a
Elvert703 <profeelvert@gmail.com>
2020-10-09 14:15:38 -0600

    Mi segunda Actualización

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejemplo</title>
+</head>
+<body>
+	<h1>Ejemplo POO - PHP</h1>
+	Operaciones básicas con 2 valores numericos(+, -, *, /)
+	<form action="operaciones.php" method="get">
+		<label for="txtValor1">Valor 1:</label>
+		<input type="text" name="txtValor1">
+		<br>
+		<label for="txtValor2">Valor 2:</label>
+		<input type="text" name="txtValor2">
+		<br>
+		<input type="submit" name="btnSuma" value="+">
+		<input type="submit" name="btnResta" value="-">
+		<input type="submit" name="btnMulti" value="*">
+		<input type="submit" name="btnDivi" value="/">
+
+	</form>
+</body>
+</html>
